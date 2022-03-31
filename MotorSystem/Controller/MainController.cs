edd00a6dbfee2084792746af7227ea8437b10e5d
daki4a <58721737+daki4a@users.noreply.github.com>
2022-03-31 14:15:36 +0300

    Add files via upload

---
+++ MotorSystem/Controller/MainController.cs
@@ -0,0 +1,31 @@
+﻿using MotorSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorSystem.Controller
+{
+    internal class MainController
+    {
+        public List<MotorDB> GetAllMotor()
+        {
+            using (MotorDBEntities1 db = new MotorDBEntities1())
+            {
+                return db.MotorDBs.ToList();
+            }
+
+        }
+        public void AddMotor(MotorDB motorDB)
+        {
+            using (MotorDBEntities1 db = new MotorDBEntities1())
+            {
+                motorDB.Id = db.MotorDBs.ToList().LastOrDefault().Id + 1;
+                db.MotorDBs.Add(motorDB);
+                db.SaveChanges();
+
+            }
+        }
+    }
+}
